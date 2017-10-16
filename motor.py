3cd46b0ff7844fb5b986ed61964958cd27a38868
fabulias <farid.abulias@gmail.com>
2017-10-15 23:47:27 -0300

    motor with state environment variable

--- motor.py
+++ motor.py
@@ -1,6 +1,11 @@
 import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
 import time                #Importamos time para poder usar time.sleep
-import sys 
+import sys
+import os
+
+if os.environ['STATE'] == "OFF":
+	GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el script
+	return
 
 portion = int(sys.argv[1]) #Asignamos las porciones ingresadas por parametro
 if portion == 0:
@@ -10,11 +15,11 @@ GPIO.setup(12,GPIO.OUT)    #Ponemos el pin 12 como salida
 p = GPIO.PWM(12,50)        #Ponemos el pin 12 en modo PWM y enviamos 50 pulsos por segundo
 p.start(2.5)               #Enviamos un pulso del 2.5% para centrar el servo
 try:
- for i in range(0,portion): 
+ for i in range(0,portion):
     p.ChangeDutyCycle(4.5)   #Enviamos un pulso del 3.75% para girar el servo hacia la izquierda
     time.sleep(1)           #pausa de medio segundo
-    p.ChangeDutyCycle(2.5)   #Enviamos un pulso del 2.5% para girar el servo hacia la derecha  
-    time.sleep(0.5) 
+    p.ChangeDutyCycle(2.5)   #Enviamos un pulso del 2.5% para girar el servo hacia la derecha
+    time.sleep(0.5)
  GPIO.cleanup()
 except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
     p.stop()                      #Detenemos el servo
