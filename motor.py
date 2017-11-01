820a7da8ff4125dfc54ebeb2623989a257e07e90
maximiliano beltran <maximiliano.beltran@mail.udp.cl>
2017-11-01 20:19:56 -0300

    ...indentation solved..

--- motor.py
+++ motor.py
@@ -4,18 +4,19 @@ import sys
 
 portion = int(sys.argv[1]) #Asignamos las porciones ingresadas por parametro
 if portion == 0:
-	return "ERROR: PORTION INVALID"
+    print("ERROR: PORTION INVALID")
+    sys.exit()
 GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
 GPIO.setup(12,GPIO.OUT)    #Ponemos el pin 12 como salida
 p = GPIO.PWM(12,50)        #Ponemos el pin 12 en modo PWM y enviamos 50 pulsos por segundo
 p.start(2.5)               #Enviamos un pulso del 2.5% para centrar el servo
 try:
- for i in range(0,portion): 
-    p.ChangeDutyCycle(4.5)   #Enviamos un pulso del 3.75% para girar el servo hacia la izquierda
-    time.sleep(1)           #pausa de medio segundo
-    p.ChangeDutyCycle(2.5)   #Enviamos un pulso del 2.5% para girar el servo hacia la derecha  
-    time.sleep(0.5) 
- GPIO.cleanup()
+    for i in range(0,portion):
+        p.ChangeDutyCycle(4.5)   #Enviamos un pulso del 3.75% para girar el servo hacia la izquierda
+        time.sleep(1)           #pausa de medio segundo
+        p.ChangeDutyCycle(2.5)   #Enviamos un pulso del 2.5% para girar el servo hacia la derecha
+        time.sleep(0.5)
+    GPIO.cleanup()
 except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
     p.stop()                      #Detenemos el servo
     GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el script
