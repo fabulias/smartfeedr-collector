b4b3eeba7f61d2af305a7fa83033988a215e3994
fabulias <farid.abulias@gmail.com>
2017-11-14 18:18:10 -0300

    adding more time to motor

--- motor.py
+++ motor.py
@@ -22,14 +22,13 @@ portion=int(portion*2)
 GPIO.setmode(GPIO.BOARD)   #Ponemos la Raspberry en modo BOARD
 GPIO.setup(12,GPIO.OUT)    #Ponemos el pin 12 como salida
 p = GPIO.PWM(12,50)        #Ponemos el pin 12 en modo PWM y enviamos 50 pulsos por segundo
-p.start(2.5)               #Enviamos un pulso del 2.5% para centrar el servo
+p.start(6.9)
 try:
     for i in range(0,portion):
-        p.ChangeDutyCycle(4.5)   #Enviamos un pulso del 3.75% para girar el servo hacia la izquierda
-        time.sleep(1)           #pausa de medio segundo
-        p.ChangeDutyCycle(2.5)   #Enviamos un pulso del 2.5% para girar el servo hacia la derecha
-        time.sleep(0.5)
-    GPIO.cleanup()
+        p.ChangeDutyCycle(2.5)   #Enviamos un pulso del 3.75% para girar el servo hacia la izquierda
+        time.sleep(1.5)           #pausa de medio segundo
+        p.ChangeDutyCycle(6.9)   #Enviamos un pulso del 2.5% para girar el servo hacia la derecha
+        time.sleep(1.5)
 except KeyboardInterrupt:         #Si el usuario pulsa CONTROL+C entonces...
-    p.stop()                      #Detenemos el servo
+    p.stop()                  #Detenemos el servo
     GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerramos el script
